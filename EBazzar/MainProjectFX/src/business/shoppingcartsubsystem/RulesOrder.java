2c0878b5a6e6ca6195b691f23234ad0779066ecd
Luis Fernandez <luisergio93@hotmail.com>
2015-04-16 18:29:59 -0500

    merge

---
+++ EBazzar/MainProjectFX/src/business/shoppingcartsubsystem/RulesOrder.java
@@ -0,0 +1,55 @@
+package business.shoppingcartsubsystem;
+
+import java.util.HashMap;
+import java.util.List;
+
+import business.exceptions.BusinessException;
+import business.exceptions.RuleException;
+import business.externalinterfaces.DynamicBean;
+import business.externalinterfaces.Rules;
+
+public class RulesOrder implements Rules {
+
+	@Override
+	public String getModuleName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRulesFile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void prepareData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public HashMap<String, DynamicBean> getTable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void runRules() throws BusinessException, RuleException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void populateEntities(List<String> updates) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<?> getUpdates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
